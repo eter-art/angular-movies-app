016d64f2b6e7ca88730c06b6634c79f959aace3a
Eter Artmeladze <ea@itacs.de>
2023-12-12 14:06:54 +0100

    added hooks to class DetailsComponent

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
-
+export class DetailsComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit{
+ 
+   constructor() {}
+  ngOnChanges(){
+    console.log('ngOnChanges called')
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit called')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called')
+  } 
+  
 }
