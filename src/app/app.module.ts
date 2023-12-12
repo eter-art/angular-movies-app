b5a8d7ed066743c62567d5a8545e56b24af2f79f
Eter Artmeladze <ea@itacs.de>
2023-12-12 13:09:50 +0100

    added more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './components/test/test.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailsComponent } from './details/details.component';
+import { DetailsMoviesComponent } from './details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './details-reviews/details-reviews.component';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
+import { SearchComponent } from './search/search.component';
+import { LoaderComponent } from './loader/loader.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    DetailsComponent,
+    DetailsMoviesComponent,
+    DetailsReviewsComponent,
+    NotAuthorizedComponent,
+    SearchComponent,
+    LoaderComponent,
+    ModalComponent
 
   ],
   imports: [
